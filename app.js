68615a298c3bcceed551cedae311e303b1452c0d
西行寺幽幽子 <HGK-SaigyoujiYuyuko@outlook.com>
2019-01-06 20:11:25 +0800

    CG: 加载耗时为毫秒 添加调试

--- app.js
+++ app.js
@@ -109,7 +109,9 @@ http.createServer(app).listen(Setting.Http_server_port,Setting.Http_server_ip);
 module.exports = app;
 
 console.log("[ " + "HttpServer".green + " ] " + "Http服务器已启动 [".input + Setting.Http_server_ip + ":" + Setting.Http_server_port + "]".input);
-console.log("[" + " Global ".green + "] " + "探针已加载完成! 耗时:".input + process.uptime() + "s 感谢您使用 SSProbe 服务器探针!".input);
+console.log("[" + " Global ".green + "] " + "探针已加载完成! 耗时: ".input + (process.uptime()*1000) + "ms 感谢您使用 SSProbe 服务器探针!".input);
+
+console.log(process.argv[3]);
 
 // 如果此次启动是测试
 if(process.argv[3] == "--test"){
