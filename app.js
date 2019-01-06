2b4a375df67f2ebd01fe7b942532748f4e93c99d
西行寺幽幽子 <HGK-SaigyoujiYuyuko@outlook.com>
2019-01-06 20:17:01 +0800

    Fix: 无法获取参数

--- app.js
+++ app.js
@@ -111,12 +111,10 @@ module.exports = app;
 console.log("[ " + "HttpServer".green + " ] " + "Http服务器已启动 [".input + Setting.Http_server_ip + ":" + Setting.Http_server_port + "]".input);
 console.log("[" + " Global ".green + "] " + "探针已加载完成! 耗时: ".input + (process.uptime()*1000) + "ms 感谢您使用 SSProbe 服务器探针!".input);
 
-console.log(process.argv);
-
 // 如果此次启动是测试
-if(process.argv[3] == "--test"){
+if(process.argv[2] == "--test"){
     console.log("[" + " Test ".debug + "] " + "检测到此次启动是测试! 系统自动退出!".input);
     console.log("Exiting...");
     
-    process.exit();
+    process.exit(0);
 }
